ecd778096ed75bca9fd5ac42433907a5396e2d6a
butch0012 <elnathanmburu@gmail.com>
2023-12-17 19:49:49 +0300

    num_reviews

--- Thirddeliverable.py
+++ Thirddeliverable.py
@@ -7,3 +7,14 @@ class Customer:
         self.family_name = family_name
         self.customer_reviews = []
         Customer.customers.append(self)
+
+    def num_reviews(self):
+        # Returns the total number of reviews that a customer has authored
+        return len(self.customer_reviews)
+    
+    def find_by_name(cls, name):
+        # Given a string of a full name, returns the first customer whose full name matches
+        for customer in cls.customers:
+            if customer.full_name() == name:
+                return customer
+        return None
