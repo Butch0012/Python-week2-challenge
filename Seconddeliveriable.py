7039a9e7b4ba0054e77d8d708224f174506090d4
butch0012 <elnathanmburu@gmail.com>
2023-12-17 19:44:00 +0300

    test cases

--- Seconddeliveriable.py
+++ Seconddeliveriable.py
@@ -60,6 +60,24 @@ restaurant1 = Restaurant("Restaurant A")
 restaurant2 = Restaurant("Restaurant B")
 
 # Add reviews
-review1 = customer1.add_review(restaurant1, 2)
-review2 = customer1.add_review(restaurant2, 3)
-review3 = customer2.add_review(restaurant1, 4) 
+review1 = customer1.add_review(restaurant1, 4)
+review2 = customer1.add_review(restaurant2, 5)
+review3 = customer2.add_review(restaurant1, 3) 
+# Test Review class methods
+print(review1.customer.full_name())  # Output: Elnathan Mburu
+print(review1.restaurant.get_name())  # Output: Restaurant A
+
+# Test Restaurant class methods
+print(restaurant1.reviews())  # Output: [<Review object at ...>, <Review object at ...>]
+print(restaurant1.customers()[0].full_name())  # Output: Elnathan Mburu
+
+# Test Customer class methods
+print(customer1.restaurants()[0].get_name())  # Output: Restaurant A
+print(customer2.add_review(restaurant2, 4).rating)  # Output: 4
+
+# Test all_reviews and all methods
+all_reviews = Review.reviews
+all_customers = Customer.customers
+
+print(all_reviews)  # Output: [<Review object at ...>, <Review object at ...>, <Review object at ...>]
+print(all_customers)  # Output: [<Customer object at ...>, <Customer object at ...>] 
