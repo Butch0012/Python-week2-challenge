8b927a3e1799d252adb15df93e8159692bdbd09e
butch0012 <elnathanmburu@gmail.com>
2023-12-17 19:04:40 +0300

    reviewed_restaurants function

--- Seconddeliveriable.py
+++ Seconddeliveriable.py
@@ -8,3 +8,7 @@ class Customer:
         self.family_name = family_name
         self.customer_reviews = []  # List to store reviews associated with this customer
         Customer.customers.append(self)  # Add the new customer to the list
+
+    def reviewed_restaurants(self):
+        # Returns a unique list of all restaurants a customer has reviewed
+        return list(set(review.restaurant for review in self.customer_reviews))
