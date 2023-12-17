ff83194b30bd34cd1611814d834d92eaea12ed8b
butch0012 <elnathanmburu@gmail.com>
2023-12-17 18:29:07 +0300

    Add the new customer to the list

--- Firstdeliverable.py
+++ Firstdeliverable.py
@@ -3,3 +3,6 @@ class Customer:
     customers = []  # A list to store all customer instances
 
     def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        Customer.customers.append(self)
